e55a808284fe25adec6293e37469fc9c4e35f3a8
GharibSidney <sidney-gharib@hotmail.com>
2024-10-31 18:37:07 -0400

    added function from homework3

---
+++ src/data_processing.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder
+
+def get_data():
+    return  pd.read_csv('data/train.csv').values, pd.read_csv('data/labels.csv').values
+
+def data_exploration(X, y):
+    NotImplementedError("This method is not implemented yet")
+
+def feature_encoding(X):
+    """
+    One-hot encode the 'features'.
+    Input: X: features (pd.DataFrame) with shape = (45211, 16)
+    Output: X: features_encoded (pd.DataFrame) with shape = (45211, 16)
+    """
+    non_numerical_columns_names = X.select_dtypes(exclude=['number']).columns
+    le = LabelEncoder()
+    for column in non_numerical_columns_names:
+        # Only encore the columns that are not numerical 
+        X[column] = le.fit_transform(X[column])
+    print(X.head())
+
+    return X
+
+def encode_label(y):
+    """
+    Encode the 'labels' data to numerical values.
+    Input: y: labels (pd.DataFrame) with shape = (45211, 1)
+    Output: y: labels_int (pd.DataFrame) with shape = (45211, 1)
+    """
+    le = LabelEncoder()
+    y['y'] = le.fit_transform(y['y'])
+    return y
+
+    
+def data_preprocessing():
+    # First download data
+    X, y = get_data()
+    # convert categorical to numerical
+    X = feature_encoding(X)
+    y = encode_label(y)
+
+    return X, y
+
+def normalize_features(X_train, X_test, Is_Standard_scaler = True):
+    """
+    Take the input data and normalize the features.
+    Input: X_train: features for train,  X_test: features for test (pd.DataFrame)
+    Output: X_train_scaled, X_test_scaled (pd.DataFrame) the same shape of X_train and X_test
+    """
+    # TODO write normalization here
+    if Is_Standard_scaler: StandardScaler()
+    else: scaler =  scaler = MinMaxScaler()
+
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    return X_train_scaled, X_test_scaled
